1910e5369a5ebefa2417f85f304cebcc96e63cbb
Jake Novelli <jakenovelli11@gmail.com>
2021-05-23 22:41:55 -0700

    added updated resume

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,7 +10,7 @@ function Footer() {
                 <a className="myLinks" target="blank" href="https://github.com/dnovelli1" alt="This will take you to my GitHub profile!"><i className="fab fa-github-square"></i></a>
                 <a className="myLinks" target="blank" href="mailto:jakenovelli11@gmail.com" alt="This will autofill my email in a draft in your email!"><i className="fas fa-envelope-square"></i></a>
                 <a className="myLinks" href="callto:17076852450" alt="My phone number is 7076852450"><i className="fas fa-phone-square"></i></a>
-                <a className="myLinks" href="./assets/resume/JakeNovelliResume.docx.pdf" download="Resume.pdf" target="blank"><i className="far fa-file"></i></a>
+                <a className="myLinks" href="./JakeNovelliResume.docx.pdf" download="Resume.pdf" target="blank"><i className="far fa-file"></i></a>
             </div>
         </footer>
     )
